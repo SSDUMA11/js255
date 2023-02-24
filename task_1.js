af00571182696f2075f14ddcac0cee55656dc102
SSDUMA11 <dumasofia2@gmail.com>
2023-02-24 17:34:48 +0200

    task_4

--- task_1.js
+++ task_1.js
@@ -2,11 +2,11 @@ let user = {}
 console.log(user);
 
 Object.defineProperties(user, {
-    name: { value: "Max", writable: false, enumerable: true, },
-    age: { value: "25", writable: false, enumerable: true, },
-    id: {value: 123, configurable: false,}
-  });
-    console.log(Object.getOwnPropertyDescriptor(user, 'name'));
-    console.log(Object.getOwnPropertyDescriptor(user, 'age'));
-    console.log(Object.getOwnPropertyDescriptor(user, 'id'));
+  name: { value: "Max", writable: false, enumerable: true, },
+  age: { value: "25", writable: false, enumerable: true, },
+  id: {value: 123, configurable: false,},
+});
+console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+console.log(Object.getOwnPropertyDescriptor(user, 'age'));
+console.log(Object.getOwnPropertyDescriptor(user, 'id'));
  
--- task_4.js
+++ task_4.js
@@ -3,22 +3,14 @@ let user = {
     surname:'Devis',
     age:25,
 }   
-//     get userInfo(){
-//         for (const [key, value] of Object.entries(user)) {
-//             console.log(`${key}: ${value}`);
-//         }
-//     }
-// }, 
-// user.userInfo;
-// user.login = 'MK_18';
-// user.userInfo;
 function getObjectProperties(obj) {
     let result = '';
     for (let prop in obj) {
-    result += (`${prop} : ${obj[prop]`)
-    },
-    
+    result += (`${prop}: ${obj[prop]},`)
+    }
     return result.slice(0, -1);
 };
-    
-    console.log(getObjectProperties(user))
+console.log(getObjectProperties(user));
+user.login = 'MK_18';
+console.log(getObjectProperties(user));
+
